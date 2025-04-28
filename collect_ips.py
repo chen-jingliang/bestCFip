fd5410e72cdf992656eb38dd73330a6339182f37
moooooss <inferno904011@gmail.com>
2025-04-28 12:12:49 +0800

    collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,8 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://ip.164746.xyz'
+        'https://ip.164746.xyz', 
+        'https://ipdb.030101.xyz/bestcfv4/'
         ]
 
 # 正则表达式用于匹配IP地址
